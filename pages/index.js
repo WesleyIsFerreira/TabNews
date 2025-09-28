d145aecc1bd697853fbe2fb50b31230ead9808ca
wesley ferreira <wdias9122@gmail.com>
2025-09-28 13:36:37 -0300

    Mudando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return 'Olá Mundo!'
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <img 
+        src="https://images.emojiterra.com/google/android-11/512px/2764.png" 
+        alt="Minha imagem" 
+        width={200} 
+        style={{ marginBottom: "20px" }}
+      />
+      <p>Te amo branquela</p>
+    </div>
+  );
 }
 
 export default Home;
